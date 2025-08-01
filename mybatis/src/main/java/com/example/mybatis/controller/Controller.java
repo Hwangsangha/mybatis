34d2e1bbb122184692f9c93ed9b138b0a856e874
User <wsh715@gmail.com>
2025-08-01 23:59:26 +0900

    html부분 만드는 중

--- mybatis/src/main/java/com/example/mybatis/controller/Controller.java
+++ mybatis/src/main/java/com/example/mybatis/controller/Controller.java
@@ -5,29 +5,27 @@ import java.util.List;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.mybatis.domain.User;
 import com.example.mybatis.mapper.UserMapper;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping("/users")
+@org.springframework.stereotype.Controller
 @RequiredArgsConstructor	//생성자 자동생성 (fianl 붙은 필드를 주입받음)
 public class Controller {
 	
 	private final UserMapper userMapper;	// MyBatis Mapper 주입
 	
-	@GetMapping
+	@GetMapping("/users")
 	public String getUserList(Model model) {
-		List<User> userList = userMapper.findAll();
-		model.addAttribute("users", userList);
+		List<User> users = userMapper.findAll();
+		model.addAttribute("users", users);
 		return "userList";
 	}
 	
@@ -44,10 +42,10 @@ public class Controller {
 	}
 	
 	//사용자 등록 (POST /users)
-	@PostMapping
-	public String addUser(@RequestBody User user) {
+	@PostMapping("/users")
+	public String addUser(@ModelAttribute User user) {
 		userMapper.insert(user);
-		return "등록 성공";
+		return "redirect:/users";
 	}
 	
 	//사용자 수정
@@ -63,5 +61,11 @@ public class Controller {
 	public String deleteUser(@PathVariable("id") int id) {
 		int result = userMapper.deleteById(id);
 		return result > 0 ? "삭제성공" : "삭제실패";
+	}
+	
+	@GetMapping("/users/new")
+	public String showUserForm(Model model) {
+		model.addAttribute("user", new User());	//빈 user 객체 전달
+		return "userForm";	//위 html파일을 렌더링
 	}
 }
